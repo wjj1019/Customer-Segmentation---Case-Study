ca6689ded54aa4c1c5d0e0ff08839fbe21fe513c
wjj1019 <70126562+wjj1019@users.noreply.github.com>
2021-11-11 23:57:08 -0500

    Update K-Mode Algorithm.py

--- K-Mode Selection (Customer Distribution)/K-Mode Algorithm.py
+++ K-Mode Selection (Customer Distribution)/K-Mode Algorithm.py
@@ -3,6 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import pyplot
 import seaborn as sns
+from functools import reduce
+from scipy import stats
+
 from kmodes.kmodes import KModes
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import SelectKBest
@@ -10,21 +13,18 @@ from sklearn.feature_selection import chi2
 
 
 # Feature selection - Use of Chi-squared as method of selection (Dataset consists of Categorical Variables)
-def select_features(X_train, y_train, X_test):
+def select_features(X, y):
     fs = SelectKBest(score_func=chi2, k='all')
-    fs.fit(X_train, y_train)
-    X_train_fs = fs.transform(X_train)
-    X_test_fs = fs.transform(X_test)
-    return X_train_fs, X_test_fs, fs
+    fs.fit_transform(X_train, y_train)
+
+    return fs
 
 # Selecting best K for the K-Mode algorithm, Elbow method plot allow visualization of best k
 def selectK (features, predictor):
     X = features
     y = predictor
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=101)
-    
     #Apply Feature Selection Function to the dataset 
-    X_train_fs, X_test_fs, fs = select_features(X_train,y_train, X_test)
+    fs = select_features(X,y)
     
     #Printing Score
     for i in range(len(fs.scores_)):
@@ -33,13 +33,32 @@ def selectK (features, predictor):
     #Plot Elbow Curve
     pyplot.bar([i for i in range(len(fs.scores_))], fs.scores_)
     return pyplot.show()
-  
+
+#Selecting Top 5 Features  
+def top5_features(fs, dataset):
+    
+    top_5_ind = []
+    for key, val in sorted(zip(fs.scores_,range(len(fs.scores_))), reverse=True):
+        top_5_ind.append([key, val])
+        top_5_ind = top_5_ind[:5]
+        
+    top_feat = {}
+    top_feat_ls = []
+    for i in range(len(top_5_ind)):
+        top_feat[dataset.columns[top_5_ind[i][1]]] = top_5_ind[i][0]
+        top_feat_ls.append(dataset.columns[top_5_ind[i][1]])
+        df = dataset[top_feat_ls]
+
+        print('Top5 Feature:{},{}'.format(dataset.columns[top_5[i][1]], top_5_ind[i][0]))
+    
+    return df
+
 # Function that provides a Elbow Plot to illustrate the best K selection
 def k_mode (dataset, num_k):
     cost = []
     K = range(1,num_k) # will be looping  K = 1 -> 5
     for num_clusters in list(K):
-        kmode = KModes(n_clusters=num_clusters, init = "random", n_init = 5, verbose=1)
+        kmode = KModes(n_clusters=num_clusters, init = "random", n_init = 5, verbose=0)
         kmode.fit_predict(dataset)
         cost.append(kmode.cost_) #Cost Function for each K
     
@@ -48,23 +67,22 @@ def k_mode (dataset, num_k):
     plt.ylabel('Cost')
     plt.title('Elbow Method For Optimal k')
     return plt.show()
-  
 
 #When the optimal K is found, create a dataframe
 def k_mode_compute__(dataset,num_k):
-    kmode = KModes(n_clusters=numk, init = "random", n_init = 5, verbose=1)
+    kmode = KModes(n_clusters=num_k, init = "random", n_init = 5, verbose=0)
     clusters = kmode.fit_predict(dataset)
     a = pd.DataFrame(clusters)
+    
     return a.iloc[:,0].value_counts().to_frame(), a.reset_index()
   
-  
-  #Simulation for 100 times - Averaging the 100 Simulation K-Mode Algorithm's Selection
-  def simulation(section, num_k):
-    freq = [] # Value_counts for 3 clusters resulted from given dataframe (Question Sections)
-    sample = [] # 5800 Samples corresponds to the value_counts df location
+#Monte Carlo Simulation
+def simulation(input_dataframe, num_k, num_iteration):
+    freq = [] # Value_counts for n_clusters (Total No. of Samples for Each Cluster for Each iteration)
+    sample = [] # 5800 Samples with label- nth_Cluster
     
-    for _ in range(0,100):
-        km = k_mode_compute__(section,num_k)
+    for _ in range(0,num_iteration):
+        km = k_mode_compute__(input_dataframe,num_k)
         freq.append(km[0])
         sample.append(km[1])
     
@@ -73,17 +91,45 @@ def k_mode_compute__(dataset,num_k):
     result = reduce(lambda df_left,df_right: pd.merge(df_left, df_right, 
                                             left_index=True, right_index=True, #on=['index'],
                                             how='outer'),data_frames)
+    return freq, sample, result
+
+#Graphical Visualization of Simulation Result
+def monte_carlo_vis(simulation_result, nth_cluster):
+    #Normal Distribution Tranformation
+    data = simulation_result.iloc[nth_cluster].values
     
+    fit_data, fit_lambda = stats.boxcox(data)
+    
+    mean = int(np.mean(data))
+    std = int(np.std(data))
+    
+    area = []
+    for ind, val  in enumerate(data):
+        if val in range(mean-std, mean+std):
+            area.append(ind)
+    
+    
+    original = sns.distplot(fit_data, hist = False, kde = True,
+            kde_kws = {'shade': True, 'linewidth': 2}, color ="green")
+    
+    near_mean = sns.distplot(fit_data[area], hist = False, kde = True,
+            kde_kws = {'shade': True, 'linewidth': 2}, color ="red")
+    
+    return original, near_mean
+
+# Finding the Opitmal Clustering Combination Obtained from the Simulation
+def optimal_cluster(simulation_result, sample):
+
     cl1 = []
     cl2 = []
     cl3 = []
     #Computing the Distance from the Mean (Finding the Optimal Cluster)
-    for i in result.iloc[0].values:
-        cl1.append(abs(i - result.iloc[0].mean()))
-    for j in result.iloc[1].values:
-        cl2.append(abs(j - result.iloc[1].mean()))
-    for k in result.iloc[2].values:
-        cl3.append(abs(k - result.iloc[2].mean()))
+    for i in simulation_result.iloc[0].values:
+        cl1.append(abs(i - simulation_result.iloc[0].mean()))
+    for j in simulation_result.iloc[1].values:
+        cl2.append(abs(j - simulation_result.iloc[1].mean()))
+    for k in simulation_result.iloc[2].values:
+        cl3.append(abs(k - simulation_result.iloc[2].mean()))
     
     final = zip(cl1,cl2,cl3)
     final_ = [round(sum(item),2) for item in final]
